18986f1e1d4cda5fa381e6615b07a70efac4e397
Tomer Almog <tomeralmog@gmail.com>
2020-03-14 16:15:14 -0700

    fix: autofocus

--- app/components/intro/ZipFinderWrapper/index.js
+++ app/components/intro/ZipFinderWrapper/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Input from '@material-ui/core/Input';
@@ -70,6 +70,10 @@ function ZipFinderWrapper({
   const [zip, setZip] = useState('');
   const [valid, setValid] = useState(false);
   const [findGeoLocation, setFindGeoLocation] = useState(false);
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [inputRef]);
 
   const onChangeText = event => {
     const text = event.target.value;
@@ -100,13 +104,15 @@ function ZipFinderWrapper({
       <Nav />
       <Wrapper>
         <StyledH2>Enter your zip to see your relevant elections</StyledH2>
-        <Body>Enter Home Zip Code</Body>
+        <Body className="bold600">Enter Home Zip Code</Body>
         <Form noValidate onSubmit={handleSubmitForm}>
           <StyledInput
             value={zip}
             onChange={onChangeText}
             maxLength={5}
             autoFocus
+            inputProps={{ autoFocus: true }}
+            inputRef={inputRef}
             type="tel"
             placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;"
           />
@@ -129,7 +135,7 @@ function ZipFinderWrapper({
         )}
         <ButtonWrapper onClick={handleNextStep}>
           <NextButton active={valid}>
-            <Next className={valid ? 'active' : ''}>SEE DISTRICT</Next>
+            <Next className={valid ? 'active' : ''}>GO</Next>
           </NextButton>
         </ButtonWrapper>
       </Wrapper>
