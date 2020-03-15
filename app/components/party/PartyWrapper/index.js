d5f2502ec807d1ddc8fed707355e46c3b5724f70
Tomer Almog <tomeralmog@gmail.com>
2020-03-15 14:14:31 -0700

    feat: learn more to slide carousel on party page

--- app/components/party/PartyWrapper/index.js
+++ app/components/party/PartyWrapper/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import Collapse from '@material-ui/core/Collapse';
 import Wrapper from 'components/shared/Wrapper';
 import MobileHeader from 'components/shared/navigation/MobileHeader';
 import Nav from 'containers/shared/Nav';
 import { Body13, H3 } from 'components/shared/typogrophy/index';
-import IntroCarousel from 'components/intro/ThreeStepsWrapper/IntroCarousel';
+import IntroCarousel from 'components/intro/ThreeStepsWrapper/CarouselLoadable';
 import EventSnippet from 'components/shared/EventSnippet';
 import articlesHelper from 'helpers/articlesHelper';
 import TopQuestions from 'components/shared/TopQuestions';
@@ -28,9 +29,19 @@ const StyledBody13 = styled(Body13)`
   color: ${({ theme }) => theme.colors.blue};
 `;
 
+const LearnMore = styled(Body13)`
+  color: ${({ theme }) => theme.colors.blue};
+  text-align: right;
+  cursor: pointer;
+`;
+
 const PartyWrapper = ({ content }) => {
+  const [showCarousel, setShowCarousel] = useState(false);
+  const [slideCarousel, setSlideCarousel] = useState(false);
   const events = content ? content.events : [];
-  console.log(events);
+
+  const transitionTime = 1000;
+
   let articles = [];
   if (content && content.faqArticles) {
     articles = articlesHelper(content.faqArticles, 'party');
@@ -39,14 +50,34 @@ const PartyWrapper = ({ content }) => {
   if (content && content.partyPage) {
     mainContent = contentfulHelper(content.partyPage.content);
   }
+
+  const toggleShowCarsouel = () => {
+    setSlideCarousel(prev => !prev);
+    if (showCarousel) {
+      setTimeout(() => {
+        setShowCarousel(false);
+      }, transitionTime);
+    } else {
+      setShowCarousel(true);
+    }
+  };
   return (
     <div>
       <Nav />
       <Wrapper white>
         <MobileHeader />
-        <IntroCarousel showButton={false} />
         {content && <CmsContentWrapper>{mainContent}</CmsContentWrapper>}
 
+        <Collapse in={slideCarousel} timeout={transitionTime}>
+          {showCarousel && (
+            <div style={{ marginTop: '50px' }}>
+              <IntroCarousel showButton={false} />
+            </div>
+          )}
+        </Collapse>
+        <LearnMore onClick={toggleShowCarsouel}>
+          {showCarousel ? 'Show Less' : 'Learn More'}
+        </LearnMore>
         {events.length > 0 && (
           <EventsWrapper>
             <Row>
