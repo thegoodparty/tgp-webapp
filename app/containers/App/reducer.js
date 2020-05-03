7808df11882439282c6ebcecdaa5aef2fde9b278
Tomer Almog <tomeralmog@gmail.com>
2020-05-03 15:44:18 -0700

    feat: admin panel version

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -5,7 +5,7 @@ const initialState = {
   content: false,
   loading: false,
   error: false,
-  appVersion: '1.0.9.12',
+  appVersion: '1.0.9.13',
 };
 
 const globalReducer = (state = initialState, action) =>
