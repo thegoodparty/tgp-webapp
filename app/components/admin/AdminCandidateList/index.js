fda627c905dfb414012092c1d9984db1a064c1ec
Tomer Almog <tomeralmog@gmail.com>
2020-05-05 02:55:07 -0700

    fix: admin panel bugs

--- app/components/admin/AdminCandidateList/index.js
+++ app/components/admin/AdminCandidateList/index.js
@@ -9,15 +9,17 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router-dom';
 
-import { partyResolver } from 'helpers/electionsHelper';
+import { candidateRoute, partyResolver } from 'helpers/electionsHelper';
 import { H3 } from '../../shared/typogrophy';
 
 const Wrapper = styled.div`
   padding: 16px;
+  overflow-x: auto;
 `;
 
 const Title = styled(H3)`
@@ -25,9 +27,23 @@ const Title = styled(H3)`
   text-align: center;
 `;
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
+const ColoredText = styled.span`
+  color: ${({ theme }) => theme.colors.gray4};
+  &.green {
+    color: ${({ theme }) => theme.colors.green};
+  }
+
+  &.red {
+    color: ${({ theme }) => theme.colors.red};
+  }
+`;
+
+const StyledSelect = styled(Select)`
+  && {
+    .MuiOutlinedInput-input {
+      padding: 8px 32px 8px 14px;
+    }
+  }
 `;
 
 const headerStyle = {
@@ -46,11 +62,15 @@ function AdminCandidateList({ candidates, updateCandidateCallback, chamber }) {
           name: candidate.name,
           party: partyResolver(candidate.party),
           incumbent: candidate.isIncumbent ? 'yes' : 'no',
+          isIncumbent: candidate.isIncumbent,
           isAligned: candidate.isAligned,
+          chamber: candidate.chamber,
+          isGood: candidate.isGood,
+          isBigMoney: candidate.isBigMoney,
         };
         if (chamber !== 'presidential') {
           fields.state = candidate.state
-            ? candidate.state.toUpperCase()
+            ? candidate.state.toLowerCase()
             : 'N/A';
         }
         if (chamber === 'house') {
@@ -71,8 +91,11 @@ function AdminCandidateList({ candidates, updateCandidateCallback, chamber }) {
     },
     {
       Header: 'Name',
-      accessor: 'name', // accessor is the "key" in the data
       headerStyle,
+      Cell: row => {
+        const route = candidateRoute(row.original);
+        return <Link to={route}>{row.original.name}</Link>;
+      },
     },
     {
       Header: 'Party',
@@ -86,35 +109,55 @@ function AdminCandidateList({ candidates, updateCandidateCallback, chamber }) {
       maxWidth: 130,
     },
     {
+      Header: 'FTM($)',
+      accessor: 'ftm',
+      headerStyle,
+      maxWidth: 130,
+      Cell: row => {
+        const { isGood, isBigMoney } = row.original;
+        let type;
+        let color;
+        if (isGood && isBigMoney) {
+          type = 'Major';
+          color = 'green';
+        } else if (isGood && !isBigMoney) {
+          type = 'Minor';
+          color = 'green';
+        } else if (!isGood && isBigMoney) {
+          type = 'Bad';
+          color = 'red';
+        } else {
+          type = 'Unknown';
+          color = 'gray';
+        }
+        return <ColoredText className={color}>{type}</ColoredText>;
+      },
+    },
+    {
       Header: 'Is Aligned',
-      filterable: false,
-      sortable: false,
+      accessor: 'isAligned',
+      headerStyle,
+      maxWidth: 180,
       Cell: row => {
         return (
-          <RadioGroup
-            aria-label="is aligned"
-            name="isAligned"
-            value={row.original.isAligned}
-            onChange={(e, newVal) => updateAlignment(row.original, newVal)}
-          >
-            <Row>
-              <FormControlLabel
-                value="unknown"
-                control={<Radio />}
-                label="Unknown"
-              />
-              <FormControlLabel
-                value="yes"
-                control={<Radio />}
-                label="Aligned"
-              />
-              <FormControlLabel
-                value="no"
-                control={<Radio />}
-                label="Not Aligned"
-              />
-            </Row>
-          </RadioGroup>
+          <FormControl variant="outlined">
+            <StyledSelect
+              value={row.original.isAligned}
+              onChange={event =>
+                updateAlignment(row.original, event.target.value)
+              }
+            >
+              <MenuItem value="unknown">
+                <em>Unknown</em>
+              </MenuItem>
+              <MenuItem value="yes">
+                <em>Aligned</em>
+              </MenuItem>
+              <MenuItem value="no">
+                <em>Not Aligned</em>
+              </MenuItem>
+            </StyledSelect>
+          </FormControl>
         );
       },
     },
