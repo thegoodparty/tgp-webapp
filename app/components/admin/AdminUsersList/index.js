a24687a0f00ee0e9042a2a48c8a98464c0689744
Tomer Almog <tomeralmog@gmail.com>
2020-06-15 22:58:58 -0700

    feat: admin user updates

--- app/components/admin/AdminUsersList/index.js
+++ app/components/admin/AdminUsersList/index.js
@@ -9,8 +9,10 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { H3 } from '../../shared/typogrophy';
+import UserAvatar from '../../shared/UserAvatar';
 
 const Wrapper = styled.div`
   padding: 16px;
@@ -26,6 +28,13 @@ const headerStyle = {
   fontWeight: 700,
   fontSize: '1.05em',
 };
+
+const NameWrapper = styled.div`
+  color: ${({ theme }) => theme.colors.blue};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 function AdminUsersList({ users }) {
   const [tableData, setTableData] = useState([]);
@@ -76,12 +85,29 @@ function AdminUsersList({ users }) {
       accessor: 'name',
       headerStyle,
       filterMethod: customFilter,
+      Cell: row => {
+        return (
+          <NameWrapper>
+            <span>{row.original.name}</span>
+            {row.original.avatar && (
+              <UserAvatar user={row.original} size="sm" />
+            )}
+          </NameWrapper>
+        );
+      },
     },
     {
       Header: 'Email',
       accessor: 'email',
       headerStyle,
       filterMethod: customFilter,
+      Cell: row => {
+        return (
+          <Tooltip title={row.original.email}>
+            <a href={`mailto:${row.original.email}`}>{row.original.email}</a>
+          </Tooltip>
+        );
+      },
     },
     {
       Header: 'Phone',
@@ -94,26 +120,44 @@ function AdminUsersList({ users }) {
       accessor: 'feedback',
       headerStyle,
       filterMethod: customFilter,
+      Cell: row => {
+        return (
+          <div style={{ overflow: 'auto', height: '100%' }}>
+            {row.original.feedback}
+          </div>
+        );
+      },
     },
     {
       Header: 'State',
       accessor: 'shortState',
       headerStyle,
       filterMethod: customFilter,
+      maxWidth: 80,
     },
     {
       Header: 'District',
       accessor: 'districtNumber',
       headerStyle,
       filterMethod: customFilter,
+      maxWidth: 80,
+      Cell: row => {
+        let { districtNumber } = row.original;
+        if (districtNumber > 0 && districtNumber < 10) {
+          districtNumber = '0' + districtNumber;
+        }
+        return <div>{districtNumber}</div>;
+      },
     },
     {
-      Header: 'Is Admin?',
+      Header: 'Admin?',
       accessor: 'isAdmin',
       headerStyle,
       filterMethod: customFilter,
+      maxWidth: 80,
     },
   ];
+  console.log('render admin');
   return (
     <Wrapper>
       <Title>All Users</Title>
