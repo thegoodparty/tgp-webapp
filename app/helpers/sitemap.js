b71946a2cb3cd3c20c7f989be68e8c3295db7101
webdeveloper001 <kd19850511@gmail.com>
2020-08-12 14:42:46 -0400

    requested update

--- app/helpers/sitemap.js
+++ app/helpers/sitemap.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDomServer from 'react-dom/server';
 import { candidateRoute } from 'helpers/electionsHelper';
 import { all } from 'redux-saga/effects';
-
+import ENV from 'api/ENV';
 const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://thegoodparty.org'
-    : 'https://dev.thegoodparty.org';
+  ENV === 'prod' ? 'https://thegoodparty.org' : 'https://dev.thegoodparty.org';
 const currentDate = new Date().toISOString().split('T')[0];
-const Animal = props => React.createElement('Animal', props);
 const UrlSet = props => React.createElement('urlset', props);
 const Url = props => React.createElement('url', props);
 const Loc = props => React.createElement('loc', props);
 const LastMod = props => React.createElement('lastmod', props);
 const ChangeFreq = props => React.createElement('changefreq', props);
-const Priority = props => React.createElement('priority', props);
-const basicLinks = [
+const staticUrls = [
   '/',
   '/home',
   '/intro/splash',
@@ -35,9 +31,10 @@ const getSitemap = candidates => {
   Object.keys(candidates).forEach(key => {
     allCandidates = [...allCandidates, ...candidates[key]];
   });
-  const BasicLinks = props => (
+  
+  const SitemapLinks = props => (
     <>
-      {basicLinks.map((link, index) => (
+      {staticUrls.map((link, index) => (
         <Url key={index}>
           <Loc>{`${baseUrl}${link}`}</Loc>
           <LastMod>{currentDate}</LastMod>
@@ -55,10 +52,10 @@ const getSitemap = candidates => {
   );
   const elementXML = ReactDomServer.renderToStaticMarkup(
     <UrlSet xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <BasicLinks />
+      <SitemapLinks />
     </UrlSet>,
   );
   return elementXML;
-}
+};
 
 export default getSitemap;
