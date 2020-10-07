140a45ce550d2c1d3e164d8ca1a924a6b7f0015b
tomer-tgp <tomer@thegoodparty.org>
2020-10-07 14:50:38 -0700

    fix: k formatter with millions

--- app/helpers/numberHelper.js
+++ app/helpers/numberHelper.js
@@ -54,6 +54,9 @@ export const toPrecision = num => {
 };
 
 export const kFormatter = num => {
+  if (Math.abs(num) > 999999) {
+    return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + 'M';
+  }
   return Math.abs(num) > 999
     ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'K'
     : Math.sign(num) * Math.abs(num);
