bd2b5c1d2eb1c1b928dc28c1255df7f22312fa66
tomer-tgp <tomer@thegoodparty.org>
2020-10-08 15:28:21 -0700

    fix: DobFormat

--- app/components/shared/customInputFormat/DobFormat.js
+++ app/components/shared/customInputFormat/DobFormat.js
@@ -1,43 +1,29 @@
-// import React from 'react';
-// import MaskedInput from 'react-text-mask';
-// 
-// const DobFormat = ({ inputRef, ...other }) => {
-//   return (
-//     <MaskedInput
-//       {...other}
-//       ref={(ref) => {
-//         inputRef(ref ? ref.inputElement : null);
-//       }}
-//       mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /[1-2]/, /\d/, /\d/, /\d/]}
-//       placeholderChar={'\u2000'}
-//       showMask  
-//     />
-//   );
-// };
-// 
-// export default DobFormat;
 import React from 'react';
 import NumberFormat from 'react-number-format';
+import PropTypes from 'prop-types';
 
-const DobFormat = ({ inputRef, onChange, ...other }) => {
-  return (
-    <NumberFormat
-      {...other}
-      getInputRef={inputRef}
-      onValueChange={(values) => {
-        onChange({
-          target: {
-            name: name,
-            value: values.value,
-          },
-        });
-      }}
-      placeholder="MM/DD/YYYY"
-      format="##/##/####"
-      mask={['M', 'M', 'D', 'D', 'Y', 'Y', 'Y', 'Y']}
-      isNumericString
-    />
-  );
+const DobFormat = ({ inputRef, onChange, ...props }) => (
+  <NumberFormat
+    {...props}
+    getInputRef={inputRef}
+    onValueChange={values => {
+      onChange({
+        target: {
+          name: values.name,
+          value: values.value,
+        },
+      });
+    }}
+    placeholder="MM/DD/YYYY"
+    format="##/##/####"
+    mask={['M', 'M', 'D', 'D', 'Y', 'Y', 'Y', 'Y']}
+    isNumericString
+  />
+);
+
+DobFormat.propTypes = {
+  inputRef: PropTypes.object,
+  onChange: PropTypes.func,
 };
 
 export default DobFormat;
