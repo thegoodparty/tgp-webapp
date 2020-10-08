31250e7cf885c8ca070c0c1cf6e6a06f75801583
tomer-tgp <tomer@thegoodparty.org>
2020-10-07 20:39:46 -0700

    chore: title align on homepage

--- app/components/home/MoneyCorruptionSection.js
+++ app/components/home/MoneyCorruptionSection.js
@@ -9,14 +9,21 @@ import { Title } from 'components/shared/typogrophy';
 const MoneyCorruptionSectionWrapper = styled.div`
   margin-top: 4rem;
   margin-bottom: 5rem;
+  padding: 20px;
 `;
 const SectionTitle = styled(Title)`
-  margin-bottom: 0.5rem;
   text-align: center;
   margin-bottom: 16px;
   @media only screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
     text-align: left;
     margin-bottom: 0;
+    padding-left: 10px;
+    font-size: 36px;
+  }
+
+  @media only screen and (min-width: 1065px) {
+    padding-left: 30px;
+    font-size: 40px;
   }
 `;
 const LeftCol = styled(Grid)`
