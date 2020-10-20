b849df9125fbb130878ddf9ded453d21a3fbcc35
tomer-tgp <tomer@thegoodparty.org>
2020-10-19 20:36:59 -0700

    feat: new challenger on home page

--- app/components/home/ChallengersSection/ChallengerAvatar.js
+++ app/components/home/ChallengersSection/ChallengerAvatar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-import DemocratImg from 'images/democrat.png';
-import RepublicanImg from 'images/republican.png';
-import IndependentImg from 'images/independent.png';
+import DemocratImg from 'images/icons/democrat.png';
+import RepublicanImg from 'images/icons/republican.png';
+import IndependentImg from 'images/icons/independent.png';
+import LibertarianImg from 'images/icons/libertarian.png';
 
 const ChallengerAvatarWrapper = styled.div`
   background: url(${props => props.avatar});
@@ -24,8 +26,10 @@ const PartyIcon = styled.img`
   border: 4px solid white;
   background: white;
   border-radius: 50%;
+  height: 45px;
+  width: 45px;
 `;
-const ChallengerAvatar = ({ avatar, party, ...props }) => {
+const ChallengerAvatar = ({ avatar, party }) => {
   let PartyImg;
   if (party === 'D') {
     PartyImg = DemocratImg;
@@ -33,12 +37,19 @@ const ChallengerAvatar = ({ avatar, party, ...props }) => {
     PartyImg = RepublicanImg;
   } else if (party === 'I') {
     PartyImg = IndependentImg;
+  } else if (party === 'L') {
+    PartyImg = LibertarianImg;
   }
   return (
     <ChallengerAvatarWrapper avatar={avatar}>
       {PartyImg && <PartyIcon src={PartyImg} />}
     </ChallengerAvatarWrapper>
   );
+};
+
+ChallengerAvatar.propTypes = {
+  avatar: PropTypes.string,
+  party: PropTypes.string,
 };
 
 export default ChallengerAvatar;
