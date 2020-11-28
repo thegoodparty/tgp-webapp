41b4c5f90e5ce46cd1b8980beb9e38e373ad3feb
tomer-tgp <tomer@thegoodparty.org>
2020-11-27 21:40:23 -0800

    fix: API_ENV

--- helpers/apiHelper.js
+++ helpers/apiHelper.js
@@ -4,7 +4,7 @@ let base;
 if (API_ENV === 'local') {
   apiBase = 'http://localhost:1337/api/v1/';
   base = 'http://localhost:1337';
-} else if (API_ENV === 'dev') {
+} else if (API_ENV === 'dev' || API_ENV === 'development') {
   apiBase = 'https://api-dev.thegoodparty.org/api/v1/';
   base = 'https://dev.thegoodparty.org';
 } else if (API_ENV === 'prod') {
