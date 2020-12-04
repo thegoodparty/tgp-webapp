e6c8f0a79f46ad477e9adaad1d21d6de808529df
tomer-tgp <tomer@thegoodparty.org>
2020-12-04 00:46:05 -0800

    fix: chamberObj null

--- containers/shared/RegisterBannerContainer/index.js
+++ containers/shared/RegisterBannerContainer/index.js
@@ -58,7 +58,7 @@ export function RegisterBannerContainer({
   } else if (houseCount > 0) {
     chamberObj = rankingObj.house[houseRankArr[0]];
   }
-  const { blocName } = chamberObj;
+  const blocName = chamberObj ? chamberObj.chamberObj : '';
   const count = presidentialCount + senateCount + houseCount;
 
   let showBanner = true;
