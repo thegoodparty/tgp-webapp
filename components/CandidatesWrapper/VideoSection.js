b884030e82a00a2c200ca16477efbcccf5f018d6
talmog <talmog@spokeo.com>
2021-03-02 18:35:57 -0800

    chore: change tiktok video

--- components/CandidatesWrapper/VideoSection.js
+++ components/CandidatesWrapper/VideoSection.js
@@ -5,9 +5,8 @@
  */
 
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Body13 } from '../shared/typogrophy';
 
 const Wrapper = styled.div`
   box-shadow: -9px 18px 30px rgba(224, 212, 234, 0.2),
@@ -18,9 +17,8 @@ const Wrapper = styled.div`
     inset -1px -1px 2px rgba(224, 212, 234, 0.5);
 `;
 
-
 const tiktokEmbed =
-  '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@scout2015/video/6718335390845095173" data-video-id="6718335390845095173" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@scout2015" href="https://www.tiktok.com/@scout2015">@scout2015</a> <p>Scramble up ur name & I’ll try to guess it😍❤️ <a title="foryoupage" target="_blank" href="https://www.tiktok.com/tag/foryoupage">#foryoupage</a> <a title="petsoftiktok" target="_blank" href="https://www.tiktok.com/tag/petsoftiktok">#petsoftiktok</a> <a title="aesthetic" target="_blank" href="https://www.tiktok.com/tag/aesthetic">#aesthetic</a></p> <a target="_blank" title="♬ original sound - tiff" href="https://www.tiktok.com/music/original-sound-6689804660171082501">♬ original sound - tiff</a> </section> </blockquote>';
+  '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@goodparty/video/6930788316031814917" data-video-id="6930788316031814917" style="max-width: 325px;min-width: 325px;"> <section> <a target="_blank" title="@goodparty" href="https://www.tiktok.com/@goodparty">@goodparty</a> <p>People of Texas: “help us!” 🥶Establishment politicians: “idgaf” 😎 <a title="texasicestorm" target="_blank" href="https://www.tiktok.com/tag/texasicestorm">#texasicestorm</a> <a title="politicians" target="_blank" href="https://www.tiktok.com/tag/politicians">#politicians</a> <a title="vote4good" target="_blank" href="https://www.tiktok.com/tag/vote4good">#vote4good</a> <a title="tx" target="_blank" href="https://www.tiktok.com/tag/tx">#tx</a> <a title="cruz" target="_blank" href="https://www.tiktok.com/tag/cruz">#cruz</a> <a title="goodparty" target="_blank" href="https://www.tiktok.com/tag/goodparty">#goodparty</a></p> <a target="_blank" title="♬ Oh No - Kreepa" href="https://www.tiktok.com/music/Oh-No-6586947002464996102">♬ Oh No - Kreepa</a> </section> </blockquote> ';
 function VideoSection() {
   useEffect(() => {
     const script = document.createElement('script');
