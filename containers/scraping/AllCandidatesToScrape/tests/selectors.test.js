76f36b9cdb49a0c2170c074fd25d82850f480dde
talmog <talmog@spokeo.com>
2021-04-17 16:10:37 -0700

    chore: remove front end scraping

--- containers/scraping/AllCandidatesToScrape/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectAllCandidatesToScrapeDomain } from '../selectors';
-
-describe('selectAllCandidatesToScrapeDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
