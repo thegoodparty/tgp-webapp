6a65c75c684d60d5f5df6778188677a802afb80f
talmog <talmog@spokeo.com>
2021-05-06 12:50:26 -0700

    chore: autoplay speed

--- components/HomePageWrapper/TestimonialsSection.js
+++ components/HomePageWrapper/TestimonialsSection.js
@@ -169,7 +169,7 @@ const TestimonialsSection = () => {
     dots: true,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 6000,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
