3f6cf4aa2aa2a53ed37130f32ae013d2653f3182
talmog <talmog@spokeo.com>
2021-05-06 12:39:19 -0700

    chore: Design and content change to Featured campaigns header

--- components/HomePageWrapper/StyledH3.js
+++ components/HomePageWrapper/StyledH3.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 const StyledH3 = styled.h3`
-  font-size: 19px;
-  line-height: 25px;
+  font-size: 16px;
+  line-height: 22px;
   margin-bottom: 24px;
-  margin-top: 8px;
+  margin-top: 4px;
   color: ${({ theme }) => theme.colors.gray4};
   font-weight: 400;
 
@@ -11,6 +11,7 @@ const StyledH3 = styled.h3`
       theme.breakpointsPixels.md}) {
     font-size: 19px;
     line-height: 25px;
+    margin-top: 8px;
   }
 
   @media only screen and (min-width: ${({ theme }) =>
