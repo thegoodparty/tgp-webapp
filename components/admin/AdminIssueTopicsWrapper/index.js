f77ce4ba1fe3d16f363aa22b887a3a9c19b1bb8c
webdeveloper001 <kd19850511@gmail.com>
2021-10-29 04:16:07 -0400

    request updated

--- components/admin/AdminIssueTopicsWrapper/index.js
+++ components/admin/AdminIssueTopicsWrapper/index.js
@@ -12,224 +12,19 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
-import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
 import AdminPageWrapper from '../AdminWrapper/AdminPageWrapper';
 import { H3, H2 } from '../../shared/typogrophy';
+import AdminIssueTopic from './AdminIssueTopic';
 
 const Wrapper = styled.div`
   padding: 24px;
   max-width: ${({ theme }) => theme.breakpointsPixels.contentMax};
   margin: 0 auto;
 `;
-
-const ControlButton = styled(IconButton)`
-  && {
-    padding: 8px;
-  }
-`;
-const POSITION_STATUS = {
+const TOPIC_STATUS = {
   NONE: null,
-  CREATE: -1,
-};
-function AdminIssueTopic({
-  topicIndex,
-  topic,
-  issue,
-  addPosition,
-  updatePosition,
-  deletePosition,
-  deleteTopic,
-  updateTopic,
-}) {
-  const [form, setForm] = useState('');
-  const [topicForm, setTopicForm] = useState(null);
-  const [index, setIndex] = useState(POSITION_STATUS.NONE);
-  return (
-    <React.Fragment key={topic}>
-      <Grid item xs={12} md={6} container alignItems="center">
-        {!topicForm && (
-          <>
-            <strong>{topic}</strong>
-            <ControlButton
-              color="primary"
-              onClick={() => {
-                setTopicForm(topic);
-              }}
-            >
-              <EditIcon />
-            </ControlButton>
-            <ControlButton
-              color="primary"
-              onClick={() => {
-                deleteTopic(issue.id);
-              }}
-            >
-              <DeleteIcon />
-            </ControlButton>
-          </>
-        )}
-        {topicForm && (
-          <Box>
-            <TextField
-              fullWidth
-              size="small"
-              primary
-              name="Positions"
-              variant="outlined"
-              value={topicForm}
-              onChange={e => setTopicForm(e.target.value)}
-            />
-            <Box mt={1}>
-              <Button
-                disabled={!topicForm}
-                variant="contained"
-                onClick={() => {
-                  updateTopic(topicIndex, topicForm);
-                  setTopicForm(null);
-                }}
-              >
-                Save
-              </Button>
-              &nbsp;&nbsp;
-              <Button
-                variant="text"
-                onClick={() => {
-                  setTopicForm(null);
-                }}
-              >
-                Close
-              </Button>
-            </Box>
-          </Box>
-        )}
-      </Grid>
-      <Grid item xs={12} md={6}>
-        {(issue?.positions || []).map((position, pIndex) => (
-          <Grid item container alignItems="center">
-            {pIndex !== index && (
-              <>
-                <span>{position?.name}</span>
-                <ControlButton
-                  color="primary"
-                  onClick={() => {
-                    setIndex(pIndex);
-                    setForm(position.name);
-                  }}
-                >
-                  <EditIcon />
-                </ControlButton>
-                <ControlButton
-                  color="primary"
-                  onClick={() => {
-                    deletePosition(topicIndex, position.id);
-                  }}
-                >
-                  <DeleteIcon />
-                </ControlButton>
-              </>
-            )}
-            {pIndex === index && (
-              <>
-                <TextField
-                  fullWidth
-                  size="small"
-                  primary
-                  name="Positions"
-                  variant="outlined"
-                  value={form}
-                  onChange={e => setForm(e.target.value)}
-                />
-                <Box mt={1}>
-                  <Button
-                    variant="contained"
-                    disabled={!form}
-                    onClick={() => {
-                      setIndex(POSITION_STATUS.NONE);
-                      updatePosition(topicIndex, form, pIndex);
-                    }}
-                  >
-                    Save
-                  </Button>
-                  &nbsp;&nbsp;
-                  <Button
-                    variant="text"
-                    onClick={() => {
-                      setIndex(POSITION_STATUS.NONE);
-                    }}
-                  >
-                    Close
-                  </Button>
-                </Box>
-              </>
-            )}
-            <br />
-          </Grid>
-        ))}
-        {index === POSITION_STATUS.CREATE && (
-          <>
-            <TextField
-              fullWidth
-              size="small"
-              primary
-              name="Positions"
-              variant="outlined"
-              value={form}
-              onChange={e => setForm(e.target.value)}
-            />
-          </>
-        )}
-        {index === POSITION_STATUS.CREATE && (
-          <Box mt={1}>
-            <Button
-              variant="contained"
-              disabled={!form}
-              onClick={() => {
-                setIndex(POSITION_STATUS.NONE);
-                addPosition(topicIndex, form);
-              }}
-            >
-              Save
-            </Button>
-            &nbsp;&nbsp;
-            <Button
-              variant="text"
-              onClick={() => {
-                setIndex(POSITION_STATUS.NONE);
-              }}
-            >
-              Close
-            </Button>
-          </Box>
-        )}
-        {index === POSITION_STATUS.NONE && (
-          <IconButton
-            color="secondary"
-            onClick={() => {
-              setIndex(POSITION_STATUS.CREATE);
-              setForm('');
-            }}
-          >
-            <AddIcon />
-          </IconButton>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <hr />
-      </Grid>
-    </React.Fragment>
-  );
-}
-AdminIssueTopic.propTypes = {
-  addPosition: PropTypes.func,
-  updatePosition: PropTypes.func,
-  deletePosition: PropTypes.func,
-  issue: PropTypes.object,
-  topic: PropTypes.string,
-  topicIndex: PropTypes.number,
-  deleteTopic: PropTypes.func,
-  updateTopic: PropTypes.func,
+  CREATE: '',
 };
 function AdminIssueTopicsWrapper({
   createCallback,
@@ -238,7 +33,7 @@ function AdminIssueTopicsWrapper({
   deleteCallback,
 }) {
   const [issueTopics, setIssueTopics] = useState([]);
-  const [newTopic, setNewTopic] = useState(null);
+  const [newTopic, setNewTopic] = useState(TOPIC_STATUS.NONE);
   useEffect(() => {
     setIssueTopics(topics);
   }, [topics]);
@@ -278,7 +73,7 @@ function AdminIssueTopicsWrapper({
         positions: [],
       },
     ]);
-    setNewTopic(null);
+    setNewTopic(TOPIC_STATUS.NONE);
     createCallback(newTopic, []);
   };
 
@@ -326,7 +121,7 @@ function AdminIssueTopicsWrapper({
               updateTopic={updateTopic}
             />
           ))}
-          {newTopic !== null && (
+          {newTopic !== TOPIC_STATUS.NONE && (
             <Grid item xs={12} md={6}>
               <TextField
                 fullWidth
@@ -339,7 +134,7 @@ function AdminIssueTopicsWrapper({
               />
             </Grid>
           )}
-          {newTopic !== null && (
+          {newTopic !== TOPIC_STATUS.NONE && (
             <Box p={1}>
               <Button
                 disabled={!newTopic}
@@ -352,18 +147,18 @@ function AdminIssueTopicsWrapper({
               <Button
                 variant="text"
                 onClick={() => {
-                  setNewTopic(null);
+                  setNewTopic(TOPIC_STATUS.NONE);
                 }}
               >
                 Close
               </Button>
             </Box>
           )}
-          {newTopic === null && (
+          {newTopic === TOPIC_STATUS.NONE && (
             <IconButton
               color="secondary"
               size="medium"
-              onClick={() => setNewTopic('')}
+              onClick={() => setNewTopic(TOPIC_STATUS.CREATE)}
             >
               <AddIcon />
             </IconButton>
