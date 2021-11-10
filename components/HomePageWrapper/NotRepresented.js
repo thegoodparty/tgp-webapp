959c1c495b0d055171014166aa596e7762948c4b
tomer-tgp <tomer@thegoodparty.org>
2021-11-09 22:13:55 -0800

    feat: mobile not represented section

--- components/HomePageWrapper/NotRepresented.js
+++ components/HomePageWrapper/NotRepresented.js
@@ -8,30 +8,58 @@ const Wrapper = styled.div`
   padding-bottom: 48px;
 `;
 const TopSection = styled.div`
-  height: 370px;
+  min-height: 300px;
   position: relative;
-  background: url('https://assets.goodparty.org/homepage/not-rep-bg.svg') bottom
-    center no-repeat;
+  background: url('/images/homepage/crowd.svg') bottom center no-repeat;
   background-size: contain;
   margin-bottom: 48px;
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpointsPixels.md}) {
+    height: 370px;
+    background: url('/images/homepage/not-rep-bg.svg') bottom center no-repeat;
+    background-size: contain;
+  }
 `;
 
 const MixText = styled.div`
-  padding-left: 80px;
-  font-size: 25px;
-  line-height: 38px;
+  font-size: 20px;
+  line-height: 28px;
+  padding: 20px;
+  text-align: center;
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpointsPixels.md}) {
+    font-size: 25px;
+    line-height: 38px;
+    padding: 0 0 0 80px;
+    text-align: left;
+  }
   .larger {
     font-size: 30px;
     line-height: 38px;
     font-weight: 700;
   }
+
+  .mobile-break {
+    display: block;
+    @media only screen and (min-width: ${({ theme }) =>
+        theme.breakpointsPixels.md}) {
+      display: inline;
+    }
+  }
 `;
 
 const NotRepText = styled(MixText)`
-  position: absolute;
-  bottom: 10%;
-  left: 80px;
   padding: 0;
+  margin-top: 32px;
+  text-align: center;
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpointsPixels.md}) {
+    position: absolute;
+    bottom: 10%;
+    left: 80px;
+    margin-top: 0;
+    text-align: left;
+  }
 `;
 
 const Red = styled.span`
@@ -44,12 +72,25 @@ const Blue = styled.span`
   font-weight: 600;
 `;
 
+const WalkSection = styled.div`
+  display: flex;
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpointsPixels.md}) {
+    display: block;
+  }
+`;
+
 const WalkImg = styled.img`
-  position: absolute;
-  width: 20%;
+  width: 50%;
   height: auto;
-  bottom: 80px;
-  right: 80px;
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpointsPixels.md}) {
+    position: absolute;
+    width: 20%;
+    height: auto;
+    bottom: 80px;
+    right: 80px;
+  }
 `;
 
 const NotRepresented = () => {
@@ -57,23 +98,26 @@ const NotRepresented = () => {
     <Wrapper>
       <TopSection>
         <NotRepText>
-          <span className="larger">130M+ of us</span> aren’t represented by{' '}
-          <Red>Red</Red> or
-          <Blue>Blue</Blue>
+          <span className="larger mobile-break">130M+ of us</span> don’t feel
+          represented by <Red>Red</Red> or <Blue>Blue</Blue>
         </NotRepText>
       </TopSection>
       <MixText>
-        <span className="larger">But we don’t know each other</span> (and aren’t
-        organized)
-      </MixText>
-      <MixText style={{ marginTop: '48px' }}>
-        <span className="larger">
-          So we’re building free tools and a community
+        <span className="larger mobile-break">
+          But we don’t know each other
         </span>{' '}
-        <br />
-        to mobilize and vote differently
+        (and aren’t organized, yet!)
       </MixText>
-      <WalkImg src="https://assets.goodparty.org/homepage/walk-social.svg" />
+      <WalkSection>
+        <MixText style={{ marginTop: '48px', padding: '8px' }}>
+          <span className="larger">
+            So we’re building free tools and a community
+          </span>{' '}
+          <br />
+          to mobilize and vote differently
+        </MixText>
+        <WalkImg src="/images/homepage/walk-social.svg" />
+      </WalkSection>
     </Wrapper>
   );
 };
