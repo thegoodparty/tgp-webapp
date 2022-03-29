d75277ed5db56d12a6708b30ad431e24b4b39125
tomer-tgp <tomer@thegoodparty.org>
2022-03-29 16:54:30 -0700

    feat: update success screen

--- components/candidate-portal/shared/AddCampaignUpdateModal.js
+++ components/candidate-portal/shared/AddCampaignUpdateModal.js
@@ -51,13 +51,32 @@ const BottomLink = styled.span`
   cursor: pointer;
 `;
 
+const Success = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 40px 20px;
+  min-height: 50vh;
+  font-size: 36px;
+  font-weight: 700;
+  position: relative;
+`;
+
+const SuccessLogo = styled.div`
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+  text-align: center;
+`;
+
 const initialState = {
   isOpen: false,
   showVideo: false,
+  showSuccess: false,
   title: '',
   date: '',
   text: '',
-  url: '',
   youtubeId: '',
   image: '',
 };
@@ -80,12 +99,19 @@ function AddCampaignUpdateModal() {
     onChangeField('isOpen', false);
   };
 
+  const canPublish =
+    state.title !== '' && state.date !== '' && state.text !== '';
+
   const publish = () => {
     const newState = { ...state };
     delete newState.showVideo;
     delete newState.isOpen;
+    delete newState.showSuccess;
     newUpdateCallback(id, newState);
-    setState(initialState);
+    onChangeField('showSuccess', true);
+    setTimeout(() => {
+      setState(initialState);
+    }, 3000);
   };
 
   return (
@@ -99,95 +125,110 @@ function AddCampaignUpdateModal() {
         showCloseButton={false}
       >
         <Inner>
-          <Row>
-            <FontH3 style={{ margin: 0 }}>New Campaign Update</FontH3>
-            <div className="text-right">
-              <Cancel onClick={closeModal}>Cancel</Cancel>
-              <BlackButton onClick={publish}>
-                <InnerButton>Publish</InnerButton>
-              </BlackButton>
-            </div>
-          </Row>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={8}>
-              <TextField
-                name="title"
-                variant="outlined"
-                label="Title for your update"
-                value={state.title}
-                fullWidth
-                inputProps={{
-                  maxLength: 50,
-                }}
-                onChange={(e) => {
-                  onChangeField('title', e.target.value);
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <TextField
-                name="date"
-                variant="outlined"
-                value={state.date}
-                fullWidth
-                type="date"
-                onChange={(e) => {
-                  onChangeField('date', e.target.value);
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="Content"
-                variant="outlined"
-                label="What's your update?"
-                value={state.text}
-                fullWidth
-                multiline
-                rows={6}
-                onChange={(e) => {
-                  onChangeField('text', e.target.value);
-                }}
-              />
-            </Grid>
-            {state.showVideo && (
-              <Grid item xs={12}>
-                <TextField
-                  name="YouTube Id"
-                  variant="outlined"
-                  label="YouTube id"
-                  value={state.youtubeId}
-                  fullWidth
-                  onChange={(e) => {
-                    onChangeField('youtubeId', e.target.value);
-                  }}
-                />
-              </Grid>
-            )}{' '}
-            {state.image && (
-              <Grid item xs={6}>
-                <img src={state.image} className="full-image" />
-              </Grid>
-            )}
-            <Grid item xs={12}>
-              <BottomLinksWrapper>
-                {!state.showVideo && (
-                  <BottomLink
-                    onClick={() => onChangeField('showVideo', true)}
-                    style={{ marginRight: '30px' }}
-                  >
-                    Add Video
-                  </BottomLink>
-                )}
-                {!state.image && (
-                  <ImageUploadContainer
-                    uploadCallback={(image) => onChangeField('image', image)}
-                    customElement={<BottomLink>Add Image</BottomLink>}
+          {state.showSuccess ? (
+            <Success>
+              <div>🎉</div>
+
+              <div style={{ margin: '24px 0' }}>Nice job!</div>
+              <SuccessLogo>
+                <img src="/images/new-logo.svg" alt="The Good Party" />
+              </SuccessLogo>
+            </Success>
+          ) : (
+            <>
+              <Row>
+                <FontH3 style={{ margin: 0 }}>New Campaign Update</FontH3>
+                <div className="text-right">
+                  <Cancel onClick={closeModal}>Cancel</Cancel>
+                  <BlackButton onClick={publish} disabled={!canPublish}>
+                    <InnerButton>Publish</InnerButton>
+                  </BlackButton>
+                </div>
+              </Row>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={8}>
+                  <TextField
+                    name="title"
+                    variant="outlined"
+                    label="Title for your update"
+                    value={state.title}
+                    fullWidth
+                    inputProps={{
+                      maxLength: 50,
+                    }}
+                    onChange={(e) => {
+                      onChangeField('title', e.target.value);
+                    }}
                   />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <TextField
+                    name="date"
+                    variant="outlined"
+                    value={state.date}
+                    fullWidth
+                    type="date"
+                    onChange={(e) => {
+                      onChangeField('date', e.target.value);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="Content"
+                    variant="outlined"
+                    label="What's your update?"
+                    value={state.text}
+                    fullWidth
+                    multiline
+                    rows={6}
+                    onChange={(e) => {
+                      onChangeField('text', e.target.value);
+                    }}
+                  />
+                </Grid>
+                {state.showVideo && (
+                  <Grid item xs={12}>
+                    <TextField
+                      name="YouTube Id"
+                      variant="outlined"
+                      label="YouTube id"
+                      value={state.youtubeId}
+                      fullWidth
+                      onChange={(e) => {
+                        onChangeField('youtubeId', e.target.value);
+                      }}
+                    />
+                  </Grid>
+                )}{' '}
+                {state.image && (
+                  <Grid item xs={6}>
+                    <img src={state.image} className="full-image" />
+                  </Grid>
                 )}
-              </BottomLinksWrapper>
-            </Grid>
-          </Grid>
+                <Grid item xs={12}>
+                  <BottomLinksWrapper>
+                    {!state.showVideo && (
+                      <BottomLink
+                        onClick={() => onChangeField('showVideo', true)}
+                        style={{ marginRight: '30px' }}
+                      >
+                        Add Video
+                      </BottomLink>
+                    )}
+                    {!state.image && (
+                      <ImageUploadContainer
+                        uploadCallback={(image) =>
+                          onChangeField('image', image)
+                        }
+                        customElement={<BottomLink>Add Image</BottomLink>}
+                      />
+                    )}
+                  </BottomLinksWrapper>
+                </Grid>
+              </Grid>
+            </>
+          )}
         </Inner>
       </Modal>
     </>
