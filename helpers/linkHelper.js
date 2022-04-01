0dc7fd8f0416e48f058e756ef22ce1dbefd196fe
tomer-tgp <tomer@thegoodparty.org>
2022-03-31 19:53:46 -0700

    feat: better is valid url

--- helpers/linkHelper.js
+++ helpers/linkHelper.js
@@ -14,14 +14,15 @@ export const getValidImgUrl = (url) => {
   return url.replace('thegoodparty.org', 'goodparty.org');
 };
 
-export const isValidUrl = (string) => {
-  let url;
-
-  try {
-    url = new URL(string);
-  } catch (_) {
-    return false;
-  }
-
-  return url.protocol === 'http:' || url.protocol === 'https:';
+export const isValidUrl = (str) => {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ); // fragment locator
+  return !!pattern.test(str);
 };
