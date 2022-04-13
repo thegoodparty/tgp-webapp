5323af0086c364f744b44d3ab3d44268ddb56fb9
tomer-tgp <tomer@thegoodparty.org>
2022-04-13 10:36:00 -0700

    chore: text changes

--- components/AboutWrapper/index.js
+++ components/AboutWrapper/index.js
@@ -44,19 +44,19 @@ const points = [
     color: '#29ADE6',
     title: 'Launch',
     content:
-      'We provide a platform for you to meet indie grassroots candidates and for good candidates to run free campaigns. No more big dollar donations necessary.',
+      'We provide a platform for you to discover independent, grassroots candidates, and for those good candidates to run free campaigns. No more big-dollar donations are necessary.',
   },
   {
     color: '#CA2CCD',
     title: 'Grow',
     content:
-      'You can make a difference in getting good candidates elected, by simply clicking “endorse” and sharing with your friends. We will never ask you for a donation.',
+      'You can make a difference in getting good candidates elected by simply clicking “endorse” and sharing with friends. We will never ask you for a donation. Just spread the word.',
   },
   {
     color: '#F6821F',
     title: 'Win',
     content:
-      'With your help, our free and open technology is diversifying the political landscape in the U.S., by allowing good indie candidates to run and win elections without relying on big donations.',
+      'With your help, our free and open technology can create more choices and competition in U.S. elections so non-corrupt, indie candidates can run and win without relying on big donations.',
   },
 ];
 function AboutWrapper() {
