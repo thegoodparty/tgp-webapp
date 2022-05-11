9485d968c84288605d14bdf7269f56175272cf9c
tomer-tgp <tomer@thegoodparty.org>
2022-05-10 22:35:23 -0700

    fix: GTM script

--- pages/_document.js
+++ pages/_document.js
@@ -230,7 +230,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               src="https://www.googletagmanager.com/ns.html?id=GTM-M53W2ZV"
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
+              style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
         </body>
