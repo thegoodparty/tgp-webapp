551b0c01964a5f7957e116a0ef7631b00e2f30fb
tomer-tgp <tomer@thegoodparty.org>
2022-05-10 22:28:50 -0700

    feat: GTM

--- pages/_document.js
+++ pages/_document.js
@@ -59,11 +59,6 @@ class CustomDocument extends NextDocument {
             name="google-signin-client_id"
             content="28351607421-c9m6ig3vmto6hpke4g96ukgfl3vvko7g.apps.googleusercontent.com"
           />
-          {/*<link*/}
-          {/*  href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;500;600;700&display=swap"*/}
-          {/*  rel="stylesheet"*/}
-          {/*/>*/}
-
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;600;900&display=swap"
             rel="stylesheet"
@@ -177,17 +172,6 @@ class CustomDocument extends NextDocument {
           if(m[y])m[y]=function(){return g._w[y].apply(this,arguments)};
           g._v='1.2.0';
         })(window,document,window['_fs_namespace'],'script','user');
-
-          // fb pixel
-        //   !function(f,b,e,v,n,t,s)
-        // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        //   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        //   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        //   n.queue=[];t=b.createElement(e);t.async=!0;
-        //   t.src=v;s=b.getElementsByTagName(e)[0];
-        //   s.parentNode.insertBefore(t,s)}(window, document,'script',
-        //   'https://connect.facebook.net/en_US/fbevents.js');
-        //   fbq('init', '1530862867115121');
         }
         `,
             }}
@@ -215,6 +199,13 @@ class CustomDocument extends NextDocument {
           js.src = 'https://connect.facebook.net/en_US/sdk.js';
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
+        
+        // GTM
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M53W2ZV');
         `,
             }}
           />
@@ -234,6 +225,14 @@ class CustomDocument extends NextDocument {
            `,
             }}
           />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M53W2ZV"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            />
+          </noscript>
         </body>
       </Html>
     );
