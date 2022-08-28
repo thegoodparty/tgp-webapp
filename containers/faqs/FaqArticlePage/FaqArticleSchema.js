98794466afacd948b0041877a51e42caea5d10d4
tomer-tgp <tomer@thegoodparty.org>
2022-08-27 17:33:37 -0700

    fix: import

--- containers/faqs/FaqArticlePage/FaqArticleSchema.js
+++ containers/faqs/FaqArticlePage/FaqArticleSchema.js
@@ -1,30 +1,28 @@
 import React, { useContext } from 'react';
 import { JsonLd } from 'react-schemaorg';
 
-import { CandidateContext, FaqArticlePageContext } from './index';
-import { candidateRoute, partyResolver } from '../../helpers/electionsHelper';
-import contentfulHelper from '../../../helpers/contentfulHelper';
+import contentfulHelper from '/helpers/contentfulHelper';
+import { FaqArticlePageContext } from './index';
 
 export default function FaqArticleSchema() {
   const { article } = useContext(FaqArticlePageContext);
-  const {
-    title,
-    articleBody
-  } = article;
+  const { title, articleBody } = article;
 
   return (
     <JsonLd
       item={{
         '@context': 'https://schema.org',
         '@type': 'FAQPage',
-        "mainEntity": [{
-          "@type": "Question",
-          "name": title,
-          "acceptedAnswer": {
-            "@type": "Answer",
-            "text": contentfulHelper(articleBody)
-          }
-        }]
+        mainEntity: [
+          {
+            '@type': 'Question',
+            name: title,
+            acceptedAnswer: {
+              '@type': 'Answer',
+              text: contentfulHelper(articleBody),
+            },
+          },
+        ],
       }}
     />
   );
