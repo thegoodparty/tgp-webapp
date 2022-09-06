b2ad06ceb239f3520b8241dea5faa4c10d486b4b
tomer-tgp <tomer@thegoodparty.org>
2022-09-06 10:50:03 -0700

    chore: remove fb sdk

--- pages/_app.js
+++ pages/_app.js
@@ -126,13 +126,13 @@ function MyApp({ Component, pageProps }) {
 
         };
 
-          (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_US/sdk.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        //   (function(d, s, id){
+        //   var js, fjs = d.getElementsByTagName(s)[0];
+        //   if (d.getElementById(id)) {return;}
+        //   js = d.createElement(s); js.id = id;
+        //   js.src = 'https://connect.facebook.net/en_US/sdk.js';
+        //   fjs.parentNode.insertBefore(js, fjs);
+        // }(document, 'script', 'facebook-jssdk'));
         
         // GTM
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
