da4230ae9c85e19c5f4595d6f2958b260553a98a
tomer-tgp <tomer@thegoodparty.org>
2022-10-03 20:34:10 -0700

    chore: web-256 - remove styles

--- components/shared/PrivacyWrapper/index.js
+++ components/shared/PrivacyWrapper/index.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import PageWrapper from '/components/shared/PageWrapper';
-import contentfulHelper, { CmsContentWrapper } from '/helpers/contentfulHelper';
+import contentfulHelper from '/helpers/contentfulHelper';
 import { dateUsHelper } from '/helpers/dateHelper';
 
-import { H1, Body11 } from '../typogrophy';
-
-const TitleWrapper = styled.div`
-  text-align: center;
-  margin-top: 24px;
-`;
-
-const RevisionWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  margin-top: 8px;
-`;
-
-const Divider = styled.span`
-  display: inline-block;
-  height: 12px;
-  width: 2px;
-  background-color: ${({ theme }) => theme.colors.red};
-  margin: 0 6px;
-`;
 
 const PrivacyWrapper = ({ content }) => {
   return (
-    <PageWrapper white>
+    <div>
       {content && (
         <>
-          <TitleWrapper>
-            <H1 data-cy="privacy-title">{content.title}</H1>
-          </TitleWrapper>
-          <RevisionWrapper>
-            <Body11 data-cy="last-revisioin-label">Last Revision</Body11>
-            <Divider />
-            <Body11 data-cy="last-revisioin-date">
-              {dateUsHelper(content.lastModified)}
-            </Body11>
-          </RevisionWrapper>
-          <CmsContentWrapper>
-            {contentfulHelper(content.pageContent)}
-          </CmsContentWrapper>
+          <h1 data-cy="privacy-title">{content.title}</h1>
+          <div data-cy="last-revisioin-label">Last Revision</div>
+          <div data-cy="last-revisioin-date">
+            {dateUsHelper(content.lastModified)}
+          </div>
+          {contentfulHelper(content.pageContent)}
         </>
       )}
-    </PageWrapper>
+    </div>
   );
 };
 
