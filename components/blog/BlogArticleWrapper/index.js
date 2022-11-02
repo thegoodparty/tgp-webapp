388e3353e98d5105617cc96e97466a6ca69b81e7
tomer-tgp <tomer@thegoodparty.org>
2022-11-02 00:19:48 -0700

    feat: blog article top author

--- components/blog/BlogArticleWrapper/index.js
+++ components/blog/BlogArticleWrapper/index.js
@@ -12,6 +12,7 @@ import { BlogArticlePageContext } from '/containers/blog/BlogArticlePage';
 import BlogPageWrapper from '../shared/BlogPageWrapper';
 import styles from './BlogArticle.module.scss';
 import contentfulHelper from '../../../helpers/contentfulHelper';
+import { dateUsHelper } from '../../../helpers/dateHelper';
 
 function BlogArticleWrapper() {
   const { sections, article } = useContext(BlogArticlePageContext);
@@ -24,7 +25,7 @@ function BlogArticleWrapper() {
   const sectionSlug = section?.fields?.slug;
   return (
     <BlogPageWrapper sections={sections} sectionSlug={sectionSlug}>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} alignItems="center">
         <Grid item xs={12} lg={6}>
           <div className={styles.image}>
             <Image
@@ -35,9 +36,24 @@ function BlogArticleWrapper() {
           </div>
         </Grid>
         <Grid item xs={12} lg={6}>
-          <div className={styles.section}>{section.fields?.title}</div>
-          <h1 className={styles.h1}>{title}</h1>
-          <div className={styles.time}>{readingTime?.text}</div>
+          <div>
+            <div className={styles.section}>{section.fields?.title}</div>
+            <h1 className={styles.h1}>{title}</h1>
+            <div className={styles.time}>{readingTime?.text}</div>
+            <div className={styles.topAuthorWrapper}>
+              <div className={styles.authorImage}>
+                <Image
+                  src={`https:${author.fields.image.url}`}
+                  alt={mainImage.alt}
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <div className={styles.authorNameTop}>
+                {author.fields.name} &middot; {dateUsHelper(publishDate)}
+              </div>
+            </div>
+          </div>
         </Grid>
       </Grid>
       <Grid container spacing={3} className={styles.reverseGrid}>
@@ -63,7 +79,9 @@ function BlogArticleWrapper() {
             </div>
             <div>
               <div className={styles.authorName}>By {author.fields.name}</div>
-              <div className={styles.authorSummary}>{author.fields.summary}</div>
+              <div className={styles.authorSummary}>
+                {author.fields.summary}
+              </div>
             </div>
           </div>
         </div>
